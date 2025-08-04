09bdd655a90b838dd9797931ccd50e4293d2fde9
JosueBarahona <72632338+josbarmore18@users.noreply.github.com>
2025-08-04 17:24:26 -0600

    Add files via upload
    Ejercicio para calcular Salarios

---
+++ Ejercicios/Ejercicio calculadora.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionCalculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicitar al usuario el costo por hora
+            Console.Write("Ingrese el precio por hora trabajada: "); 
+            double costoPorHora = Convert.ToDouble(Console.ReadLine());
+
+            // Solicitar al usuario la cantidad de horas trabajadas
+            Console.Write("Ingrese la cantidad de horas trabajadas en la semana: ");
+            double horasTrabajadas = Convert.ToDouble(Console.ReadLine());
+
+            // Calcular el salario total d
+            double total = costoPorHora * horasTrabajadas;
+
+            // Mostrar el resultado
+            Console.WriteLine("El salario semanal es: L. " + total);
+        }
+    }
+}
